4b034588c884adb1deacecce855fe2c1c03173e3
Sandra <cheptoosandra.t@gmail.com>
2023-05-02 20:24:33 +0300

    Initial Commit

---
+++ conn.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "online_examination";
+$conn = null;
+
+try {
+//    $conn = new PDO("mysql:host={$host};dbname={$db};",$user,$pass);
+//} catch (Exception $e) {
+//
+//}
+//
+//
+?>
+
